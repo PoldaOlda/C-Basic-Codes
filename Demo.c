d5973d07563811427a109cee4a9f729c3b76a5ab
PoldaOlda <poldaolda123@gmail.com>
2024-01-12 11:28:17 +0530

    [ADDED]:Tahir - C Program for Addition of 2 No

---
+++ Demo.c
@@ -0,0 +1,13 @@
+//Program to Print Hello Message 		---> Documentation
+
+#include<stdio.h>				//----> Header Files
+
+int main()				//---> Main Function
+{
+	printf("Hello....");
+	
+	return 0;
+	
+}
+
+
